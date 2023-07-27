a554e276c5784562053e6fb78f003390b5254533
xi <xi@doxbin.gay>
2023-07-27 18:55:21 +0100

    Update autotyper.py

--- autotyper.py
+++ autotyper.py
@@ -8,12 +8,13 @@ ids = [
 
 headers = {"authorization" : token}
 print("Beginning autotyper")
+
 while True:
     for id in ids:
         endpoint = f"https://discord.com/api/v9/channels/{id}/typing"
         time.sleep(1)
         res = requests.post(endpoint,headers=headers)
         if res.status_code == 204:
-            print(f"sent to {id}")
+            print(f"Sent typing request to {id}")
         else:
-            print(f"failed to send to {id}")
+            print(f"Failed to type in {id}")
