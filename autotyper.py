c0518f9d650527b9c8eb3e0712072840368765cc
xi <vokunocode@protonmail.com>
2023-07-27 18:50:49 +0100

    Add files via upload

---
+++ autotyper.py
@@ -0,0 +1,18 @@
+import requests
+import time
+
+token = ""
+ids = [
+    1
+]
+headers = {"authorization" : token}
+print("Beginning autotyper")
+while True:
+    for id in ids:
+        endpoint = f"https://discord.com/api/v9/channels/{id}/typing"
+        time.sleep(1)
+        res = requests.post(endpoint,headers=headers)
+        if res.status_code == 204:
+            print(f"sent to {id}")
+        else:
+            print(f"failed to send to {id}")
