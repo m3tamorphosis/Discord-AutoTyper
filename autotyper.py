624b5c62a7a14e574655596c83c5969f5f826f80
womp <xi@doxbin.gay>
2023-08-02 18:45:50 +0100

    Update autotyper.py
    introduced multitoken support

--- autotyper.py
+++ autotyper.py
@@ -1,20 +1,32 @@
 import requests
 import time
+import os
 
-token = ""
-ids = [
-    1
+tokens =  [
+    "token" # seperate with comma
 ]
 
-headers = {"authorization" : token}
-print("Beginning autotyper")
+ids = [
+    1 # seperate with comma
+]
 
-while True:
-    for id in ids:
-        endpoint = f"https://discord.com/api/v9/channels/{id}/typing"
-        time.sleep(1)
-        res = requests.post(endpoint,headers=headers)
-        if res.status_code == 204:
-            print(f"Sent typing request to {id}")
-        else:
-            print(f"Failed to type in {id}")
+def startup():
+    os.system("title Autotyper v2")
+    os.system("cls")
+    print("Beginning autotyper")
+
+def main():
+    startup()
+    while True:
+        for id in ids:
+            for token in tokens:
+                headers = {"authorization" : token}
+                endpoint = f"https://discord.com/api/v9/channels/{id}/typing"
+                time.sleep(0.5)
+                res = requests.post(endpoint, headers=headers)
+                if res.status_code == 204:
+                    print(f"Sent typing request to {id} under {token[:5]}")
+                else:
+                    print(f"Failed to type in {id} under {token[:5]}")
+
+main()
