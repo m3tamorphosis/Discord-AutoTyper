360d250b648cc236279ef551e66c414e6a9df4a2
4o <xi@doxbin.gay>
2023-10-07 14:00:49 +0100

    Update autotyper.py
    Added process timer

--- autotyper.py
+++ autotyper.py
@@ -3,21 +3,31 @@ import time
 import os
 
 tokens =  [
-    "token" # seperate with comma
+    "token",
 ]
 
 ids = [
-    1 # seperate with comma
+    1,
 ]
 
+# Seperate with commas (people genuinely don't understand this)
+
 def startup():
-    os.system("title Autotyper v2")
     os.system("cls")
     print("Beginning autotyper")
 
+def update(start_time):
+    program_time = time.time() - start_time
+    hours = int(program_time // 3600)
+    minutes = int((program_time % 3600) // 60)
+    seconds = int(program_time % 60)
+    os.system(f"title Discord Autotyper - Duration: {hours}:{minutes}:{seconds}")
+
 def main():
     startup()
+    start_time = time.time()
     while True:
+        update(start_time)
         for id in ids:
             for token in tokens:
                 headers = {"authorization" : token}
@@ -27,6 +37,6 @@ def main():
                     print(f"Sent typing request to {id} under {token[:5]}")
                 else:
                     print(f"Failed to type in {id} under {token[:5]}")
-        time.sleep(3)
+        time.sleep(1)
 
 main()
