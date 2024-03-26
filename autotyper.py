dda68067a0ca9bc71d70894c82cc4dd42dd9ceda
xi <xi@doxbin.gay>
2024-03-26 08:53:33 +0000

    Update autotyper.py
    added actual python conventions finally

--- autotyper.py
+++ autotyper.py
@@ -12,23 +12,24 @@ ids = [
 
 # Seperate with commas (people genuinely don't understand this)
 
-def startup():
+def Send_Data(id, token):
+    headers = {"authorization" : token}
+    endpoint = f"https://discord.com/api/v9/channels/{id}/typing"
+    res = requests.post(endpoint, headers=headers)
+    if res.status_code == 204:
+    print(f"Sent typing request to {id} under {token[:5]}")
+    else:
+    print(f"Failed to type in {id} under {token[:5]}")
+
+def main():
     os.system("cls")
     print("Beginning autotyper")
-    
-def main():
-    startup()
     while True:
         for id in ids:
             for token in tokens:
-                headers = {"authorization" : token}
-                endpoint = f"https://discord.com/api/v9/channels/{id}/typing"
-                res = requests.post(endpoint, headers=headers)
-                if res.status_code == 204:
-                    print(f"Sent typing request to {id} under {token[:5]}")
-                else:
-                    print(f"Failed to type in {id} under {token[:5]}")
+                Send_Data(id, token)
         time.sleep(1)
 
 
-main()
+if __name__ == "__main__":
+    main()
